6d90e0803fd3fae3192f8bd66e393cec5e160ede
Alexande Laskevic <al.laskevic@gmail.com>
2025-03-21 08:45:46 +0100

    hardcoded for pitch

--- src/stores/BaseStore.js
+++ src/stores/BaseStore.js
@@ -1,3 +1,228 @@
+//temp Fix
+
+const projects = [
+  {
+    id: 1,
+    virginCompany: "Virgin Atlantic",
+    initiative: "Youngest, Cleanest Fleet in the Sky",
+    challenge:
+      "The time for action against climate change is now. Virgin Atlantic are on a mission to achieve net-zero by 2050.",
+    whatVirginIsDoing:
+      "Virgin Atlantic is working to accelerate the development of sustainable fuels. On November 28th, we made history with Flight100— becoming the first commercial airline to fly across the Atlantic on 100% SAF - marking a key milestone on the path to decarbonising aviation.",
+    callToActions: [
+      {
+        name: "Donate",
+        reward: {
+          name: null,
+          earnedBadge: {
+            name: "Cool Guy",
+            image: "/images/badge-1.png",
+            description: "This person cared..",
+          },
+          earnedXPAmount: 10,
+          earnedVirginPoints: 20,
+          description:
+            "Sint expedita nihil totam et est non velit dolorem quo.",
+        },
+        category: null,
+        rewardCode: null,
+      },
+    ],
+    links: [
+      "https://corporate.virginatlantic.com/gb/en/business-for-good/planet.html",
+      "https://corporate.virginatlantic.com/gb/en/business-for-good/planet/fuel/flight100.html\nhttps://corporate.virginatlantic.com/gb/en/business-for-good/planet/fuel.html",
+    ],
+    publishDate: "2025-03-21T08:41:36.499664+01:00",
+    imageLink:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/M_s_galaxy.jpg/640px-M_s_galaxy.jpg",
+  },
+  {
+    id: 2,
+    virginCompany: "Virgin Atlantic & Virgin Unite",
+    initiative: "Protecting our Planet",
+    challenge:
+      "Contrails, aircraft condensation trails, heighten the effect of global warming, which may account for more than half (57%) of the entire climate impact of aviation.",
+    whatVirginIsDoing:
+      "Virgin Atlantic, Virgin Unite, and Flight100 have also joined forces with RMI to establish the Contrail Impact Task Force, aiming to address the environmental impact of aircraft contrails.",
+    callToActions: [
+      {
+        name: "Stay informed",
+        reward: {
+          name: null,
+          earnedBadge: {
+            name: "Cool Guy",
+            image: "/images/badge-1.png",
+            description: "This person cared..",
+          },
+          earnedXPAmount: 10,
+          earnedVirginPoints: 20,
+          description: "Illo qui voluptas architecto eaque.",
+        },
+        category: null,
+        rewardCode: null,
+      },
+      {
+        name: "Donate To RMI",
+        reward: {
+          name: null,
+          earnedBadge: {
+            name: "Cool Guy",
+            image: "/images/badge-0.png",
+            description: "This person cared..",
+          },
+          earnedXPAmount: 10,
+          earnedVirginPoints: 20,
+          description: "Quo debitis labore quo velit.",
+        },
+        category: null,
+        rewardCode: null,
+      },
+    ],
+    links: [
+      "https://corporate.virginatlantic.com/gb/en/business-for-good/planet/fleet.html",
+      "https://www.virgin.com/virgin-unite/latest/flight100-virgin-atlantic-and-rmi-test-new-ways-to-reduce-aviations-climate",
+    ],
+    publishDate: "2025-03-21T08:41:36.5079165+01:00",
+    imageLink:
+      "https://www.science.org/do/10.5555/article.2375495/full/sf-chemtrails-1644894851440.jpg",
+  },
+  {
+    id: 3,
+    virginCompany: "Virgin Voyages",
+    initiative: "Epic Sea Change For All",
+    challenge:
+      "Wildlife havens, carbon stores, storm defences, ocean purifiers – mangrove swamps are one of the hardest-working habitats on Earth, but they’re disappearing fast.",
+    whatVirginIsDoing:
+      "Virgin Voyages have teamed up with Virgin’s Foundation, Virgin Unite, to support mangrove forest projects in the Caribbean. The aim is to accelerate nature-based solutions to climate change, and create a scalable model for other regions in the world.",
+    callToActions: [
+      {
+        name: "Get involved in a Beach Clean onboard",
+        reward: {
+          name: null,
+          earnedBadge: {
+            name: "Cool Guy",
+            image: "/images/badge-2.png",
+            description: "This person cared..",
+          },
+          earnedXPAmount: 10,
+          earnedVirginPoints: 20,
+          description: "Voluptatem temporibus qui quod quae sed enim.",
+        },
+        category: null,
+        rewardCode: null,
+      },
+      {
+        name: "Donate to Sea Change For all Fund",
+        reward: {
+          name: null,
+          earnedBadge: {
+            name: "Cool Guy",
+            image: "/images/badge-3.png",
+            description: "This person cared..",
+          },
+          earnedXPAmount: 10,
+          earnedVirginPoints: 20,
+          description: "Aut hic consequatur neque quis aut possimus ea.",
+        },
+        category: null,
+        rewardCode: null,
+      },
+    ],
+    links: ["https://www.virginvoyages.com/sustainability"],
+    publishDate: "2025-03-21T08:41:36.5115405+01:00",
+    imageLink:
+      "https://www.sailingeurope.com/blog/wp-content/uploads/2019/01/Barbados.jpg",
+  },
+  {
+    id: 4,
+    virginCompany: "Virgin Media 02",
+    initiative: "Better Connections Plan",
+    challenge:
+      "Old IT equipment can lead to electronic waste, or e-waste, polluting the environment. Recycling old IT equipment plays a vital role in preventing this. Virgin Media O2 have a zero landfill policy and will be come a zero-waste business by the end of 2025, and are committed to achieving zero waste operations and products.",
+    whatVirginIsDoing:
+      "O2 Recycle is a service launched in October 2009, which allows anyone in the UK whether an O2 customer or not, to trade in their devices and gadgets responsibly, in return for cash. The vast majority of phones we receive will be reused, repaired or recycled for parts. Since launch, the scheme has paid out more than £320 million, and sustainably recycled 3.8 million devices – with zero going to landfill.",
+    callToActions: [
+      {
+        name: "Sell your old device with O2 Recycle",
+        reward: {
+          name: null,
+          earnedBadge: {
+            name: "Cool Guy",
+            image: "/images/badge-2.png",
+            description: "This person cared..",
+          },
+          earnedXPAmount: 10,
+          earnedVirginPoints: 20,
+          description:
+            "Deleniti quo sint voluptatem hic velit quibusdam officia et ad.",
+        },
+        category: null,
+        rewardCode: null,
+      },
+      {
+        name: "Donate devices via Community Calling",
+        reward: {
+          name: null,
+          earnedBadge: {
+            name: "Cool Guy",
+            image: "/images/badge-1.png",
+            description: "This person cared..",
+          },
+          earnedXPAmount: 10,
+          earnedVirginPoints: 20,
+          description: "Dolor minus tenetur tempore vel quae a porro.",
+        },
+        category: null,
+        rewardCode: null,
+      },
+    ],
+    links: [
+      "https://www.virgin.com/about-virgin/latest/virgin-media-o2-launches-better-connections-plan",
+      "https://www.o2recycle.co.uk/",
+    ],
+    publishDate: "2025-03-21T08:41:36.5147859+01:00",
+    imageLink:
+      "https://www.hightechrecyclingmi.com/wp-content/uploads/sites/124/2019/10/printer-hero-680x266.jpg",
+  },
+];
+
+const redeem = {
+  name: null,
+  earnedBadge: {
+    name: "Cool Guy",
+    image: "/images/badge-1.png",
+    description: "This person cared..",
+  },
+  earnedXPAmount: 10,
+  earnedVirginPoints: 20,
+  description: "Sint expedita nihil totam et est non velit dolorem quo.",
+};
+
+const profile = {
+  id: 0,
+  name: "Reto Schweizmann",
+  earnedBadges: [
+    {
+      name: "Beach Cleaner",
+      image: "/images/badge-0.png",
+      description: "Cleaned a beach",
+    },
+    {
+      name: "Sugar Daddy",
+      image: "/images/badge-2.png",
+      description: "Donated to cause",
+    },
+    {
+      name: "Cool Guy",
+      image: "/images/badge-1.png",
+      description: "This person cared..",
+    },
+  ],
+  projectParticipations: [1, 2, 3],
+  xp: 210,
+  level: 3,
+};
+
 export const baseUrl = "http://192.168.178.53:5001";
 
 class BaseStore {
@@ -40,14 +265,17 @@ class BaseStore {
   }
 
   async getProjects() {
+    return projects;
     return await this._get("/projects");
   }
 
   async getUser(userId = 0) {
+    return profile;
     return await this._get(`/users/${userId}`);
   }
 
   async getRedeem(userId = 0, qrCode = "cleanfleet") {
+    return redeem;
     return await this._get(`/users/${userId}/redeem/${qrCode}`);
   }
 }
